8702999a897ea78b156d26cb99f328297f3e2452
victoriapavlova <victoria.pavlova7@gmail.com>
2023-02-16 16:44:22 +0300

    44 Context API; вытащила все в Провайдер, типизация >.<

--- src/StoreContext.tsx
+++ src/StoreContext.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
-import {StoreType} from "./redux/myStore";
+import {StoreType, TsarType} from "./redux/myStore";
 import {store} from "./redux/redux-store";
 import App from "./App";
+import {ActionTypes} from "redux-form";
+import {Store} from "redux";
 
 const StoreContext = React.createContext(store)
 
-export const Provider = (props: any) => {
+export type ProviderType = {
+    store:typeof store
+    children:React.ReactNode
+}
+export const Provider = (props: ProviderType) => {
     return (
         <StoreContext.Provider value={props.store}>
             {props.children}
